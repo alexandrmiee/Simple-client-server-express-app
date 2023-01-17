fdd152fe3e99e85ad50350c8f15c977042f40ee8
Alexander Lukovnikov <lukovnikov.alexandr@gmail.com>
2023-01-18 01:04:38 +0300

    feat(common): setup

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  vueIndentScriptAndStyle: false,
+  htmlWhitespaceSensitivity: 'ignore',
+  endOfLine: 'lf',
+  bracketSameLine: false,
+  singleAttributePerLine: true,
+};
